8a4eb8cfbb73e9f25d818f18195c46de77120bb0
AnOnYmOus001100 <47621801+AnOnYmOus001100@users.noreply.github.com>
2020-10-02 01:30:37 +0530

    adding 4 files

---
+++ C code/Basic C programs/helloworld-loop.c
@@ -0,0 +1,18 @@
+/* 
+	Author: AnOnYmOus001100
+	Date: 02/10/2020
+
+	Descripton: This program demontrates a infinite loop
+*/
+
+
+#include<stdio.h>
+
+int main()
+{
+	// the while loop condition always remains true so the loops keeps on repeating forever, until interrupted
+	while(1)
+		// prints hello world forever
+		printf ("hello world forever!");
+	return 0;
+}
---
+++ C code/Basic C programs/print1-10.c
@@ -0,0 +1,21 @@
+/* 
+	Author: AnOnYmOus001100
+	Date: 02/10/2020
+
+	Descripton: To print 1 to 10 using for loop
+*/
+
+#include<stdio.h>
+
+int main()
+
+{	
+	// variable declaration
+	int i;
+
+	// for loop starts from 1 and loops till 10
+	for (i=1;i <= 10; i++)
+		// printing the number
+		printf ("%d\n",i);
+	return 0;
+}
