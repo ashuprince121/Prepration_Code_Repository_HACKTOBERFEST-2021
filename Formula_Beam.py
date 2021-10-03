8d6411edb178dddb36240f48f5bb0efa8e5153d2
Grakody <59213454+Faiznurullah@users.noreply.github.com>
2021-10-03 13:29:36 +0700

    Change File To Beam Formula

---
+++ Formula_Beam.py
@@ -0,0 +1,8 @@
+print ("PYTHON PROGRAM CALCULATE THE VOLUME OF BEAM")
+p = float(input("long = "))
+l = float(input("wide   = "))
+t = float(input("height  = "))
+
+v = p*l*t
+
+print ("Volume Beam = %0.2f" %v)
