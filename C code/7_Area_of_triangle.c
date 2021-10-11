5dec8df2d9c31f07b3ae7ece96a0ecd2d1c82404
Abhishek Kumar Singh <92250574+abhishekuif@users.noreply.github.com>
2021-10-11 02:36:36 -0700

    Update 7_Area_of_triangle.c

--- C code/7_Area_of_triangle.c
+++ C code/7_Area_of_triangle.c
@@ -10,3 +10,21 @@
 		ZipRecovery 1.6 at:
 
 		http://ref.officerecovery.com/zip/?5316zr/
+// Area of Tringle in C
+#include <stdio.h>
+#include <math.h>
+int main()
+{
+  double a, b, c, s, area;
+
+  printf("Enter sides of a triangle\n");
+  scanf("%lf%lf%lf", &a, &b, &c);
+
+  s = (a+b+c)/2; // Semiperimeter
+
+  area = sqrt(s*(s-a)*(s-b)*(s-c));
+
+  printf("Area of the triangle = %.2lf\n", area);
+
+  return 0;
+}
