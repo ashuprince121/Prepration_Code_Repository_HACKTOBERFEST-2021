76c90d55bf1a56353457369eef067f39288ab7e1
Umakshi12 <66000862+Umakshi12@users.noreply.github.com>
2021-10-24 12:09:21 +0530

    point_star_turtle.py
    This is the python code by using the turtle library.

---
+++ Python code/Point_star_turtle.py
@@ -0,0 +1,7 @@
+#This is the python code of generating the point star by using the turtle library
+from turtle import* #import turtle library
+color("green") # Changing its colour to green
+for i in range(20):
+    forward(200)
+    left(162)
+bye()
